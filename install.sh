4e8706d4b2c0730737cd4c4156f3d2dc42154083
inatomitk <35132611+inatomitk@users.noreply.github.com>
2019-02-08 18:20:37 +0900

    .configも対象に

--- install.sh
+++ install.sh
@@ -6,6 +6,11 @@ for f in * .[^\.]*; do
   [ "$f" = ".git" ] && continue
   [ "$f" = ".DS_Store" ] && continue
   [ "$f" = "install.sh" ] && continue
+  [ "$f" = ".config" ] && continue
 
+  ln -snfv "$SCRIPT_DIR"/"$f" "$HOME"/"$f"
+done
+
+for f in .config/*; do
   ln -snfv "$SCRIPT_DIR"/"$f" "$HOME"/"$f"
 done
